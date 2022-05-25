10748492b8be3f17f441857aaa19ab635b088181
youngukkim <kimyu6358@gmail.com>
2022-05-25 23:19:21 +0900

    two sum map 1cycle

--- src/array/TwoSum.java
+++ src/array/TwoSum.java
@@ -1,22 +1,42 @@
 package src.array;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class TwoSum {
 
     public static void main(String[] args) {
-        int[] sums = {2,8,10,21,25,30};
+        int[] sums = {2,2,8,10,21,25,30};
         int target = 46;
 
         TwoSum a = new TwoSum();
-        int[] result = a.twoSum1(sums,target);
+        //int[] result = a.twoSum1(sums,target);
+        int[] result =a.twoSum2(sums, target);
 
         System.out.println(Arrays.toString(result));
 
     }
 
+    //map
+    private int[] twoSum2(int[] nums, int target) {
+
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i=0; i<nums.length; i++){
+            int subVal = target - nums[i];
+            if(map.containsKey(subVal)) {
+                return new int[] {map.get(subVal),i};
+            }
+            map.put(nums[i],i);
+        }
+
+        return  null;
+
+
+    }
+
     //brute force
-    public int[] twoSum1(int[] nums, int target) {
+    private int[] twoSum1(int[] nums, int target) {
 
         for(int i=0; i<nums.length; i++) {
 
